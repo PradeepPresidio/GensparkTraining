0b5b50044918ec90116ecd78fd1458b67d54838b
Pradeep SN <pradeepsn606@gmail.com>
2025-05-27 09:03:00 +0530

    week 4 day 1

---
+++ week 4 day 1/Controllers/SampleController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("/api/[controller]")]
+public class SampleController : ControllerBase
+{
+    [HttpGet]
+    public string GetGreet()
+    {
+        return "Hello World";
